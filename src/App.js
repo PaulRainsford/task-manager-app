f7f985f3d0182b59bc41c436778406a8186fc1b6
Paul Rainsford <paulrainsford69@gmail.com>
2023-05-16 21:25:27 +0100

    Added react features and adding task manager

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react'
 
 function TaskManager () {
+  const [tasks, setTasks] = useState([])
+  const [inputValue, setInputValue] = useState('')
+  
+  function handleChange(e){
+    setInputValue(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setTasks([...tasks, inputValue])
+    setInputValue('')
+  }
   return (
     <div>
+      <h1>Task Manager</h1>
       <form>
-        <input type="text" />
-        <button>Add Task</button>
+        <input type='text' value={inputValue} onChange={handleChange}/>
+        <button onClick={handleSubmit}>Add Task</button>
       </form>
       <ul>
-        <li>
+        {tasks.map((task) => (
+        <li key={task}>{task}
+        <button>Delete</button>
         </li>
+        ))}
       </ul>
     </div>
-  );
+  )
 }
 
 export default TaskManager;
